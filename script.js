75718aca89cc9037ebed8ad4a1355b42da0d6cec
Lionella09 <lionella.l@outlook.com>
2024-07-31 15:24:27 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+/*let prenom = "jesus t'aime";
+alert("salut "+prenom);
+
+let age=prompt("quel est ton age?");
+
+if(age>=12){
+	alert("BVN sur vinbooks");
+}
+else{
+	alert("tu es encore trop jeune pour le site");
+}
+let mot_de_passe=prompt("mot de passe");
+if(mot_de_passe=="12345678910"){
+	alert("cool");
+}
+else{
+	alert("t'a flop");
+}
+let mdp=prompt("mot de passe");
+let vrai_mdp="1234567890";
+	while(mdp!=vrai_mdp){
+		alert("flop le mdp")
+		mdp = prompt("mot de passe")
+	}
+	alert("good password");*/
+/*let prenom=prompt("what your name ?");
+let titre=document.getElementById("titre");
+titre.textContent="hola mi amigos "+prenom;
+
+let paragraphe = document.createElement("p");
+paragraphe.innerTexts "wesh";
+*/
